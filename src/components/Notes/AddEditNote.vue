253aae39d6f984f85acba32bcceb4ff0f4570e43
Mariusz <siewruk.mariusz92@gmail.com>
2023-02-10 13:49:03 +0100

    Refactored AddEditNote

--- src/components/Notes/AddEditNote.vue
+++ src/components/Notes/AddEditNote.vue
@@ -1,38 +1,26 @@
-<template>
-  <div :class="`card has-background-${cardBgType}-dark p-4 mb-5`">
-    <div class="field">
-      <div class="control">
-        <textarea
-          v-model="noteContent"
-          ref="newNoteTextAreaRef"
-          class="textarea"
-          v-bind="{ placeholder }"
-          v-autofocus
-          max-length="100"
-        />
-      </div>
-    </div>
+<template lang="pug">
+v-card.pa-2.pb-0.mb-10
+  v-textarea(
+    v-model="noteContent"
+    autofocus
+    hide-details
+    v-bind="{ placeholder }"
+  )
+  v-card-actions
+    v-btn.mx-auto(
+      :disabled="!noteContent"
+      @click="onSubmitHandler"
+    )
+      v-icon.mr-1 far fa-square-plus
+      | {{ submitLabel }}
 
-    <div class="field is-grouped is-grouped-right">
-      <div class="control">
-        <button
-          class="button is-link has-background-success"
-          :disabled="!noteContent"
-          @click="onSubmitHandler"
-        >
-          {{ submitLabel }}
-        </button>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script setup>
 // imports
-import { ref, computed, onMounted, watch } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useNotesStore } from '@/stores/useNotesStore'
-import { vAutofocus } from '@/directives/vAutofocus'
 import { useWatchCharacters } from '@/use/useWatchCharacters'
 
 const store = useNotesStore()
@@ -51,15 +39,14 @@ const props = defineProps({
 
 // initial data
 const noteContent = ref('')
-const newNoteTextAreaRef = ref(null)
 const isAdding = computed(() => props.mode === 'add')
 
 // save handling
 const onAddNewNoteHandler = () => {
   store.addNote(noteContent.value)
   noteContent.value = ''
-  newNoteTextAreaRef.value.focus()
 }
+
 const onSaveEditNoteHandler = () => {
   store.updateNote(route.params.id, noteContent.value)
   router.push('/')
@@ -76,8 +63,6 @@ const placeholder = computed(() => {
 const submitLabel = computed(() => {
   return props.mode === 'add' ? 'Add New Note' : 'Save Note'
 })
-
-const cardBgType = computed(() => (isAdding.value ? 'success' : 'link'))
 
 onMounted(() => {
   if (!isAdding.value) {
