5a30b236773f33a28665441a0aea0fd423906717
Mariusz <siewruk.mariusz92@gmail.com>
2023-02-10 12:26:10 +0100

    Refactored view auth

--- src/views/ViewAuth.vue
+++ src/views/ViewAuth.vue
@@ -1,29 +1,23 @@
-<template>
-  <Container container-style="narrow">
-    <div class="auth">
-      <div class="tabs is-centered">
-        <ul>
-          <li
-            v-for="tab in tabs"
-            :key="tab.path"
-            :class="tab.classes"
-          >
-            <RouterLink :to="tab.to">
-              {{ tab.label }}
-            </RouterLink>
-          </li>
-        </ul>
-      </div>
-      <div><RouterView /></div>
-    </div>
-  </Container>
+<template lang="pug">
+Container
+  v-tabs.mb-2(
+    :value="route.name"
+    align-tabs="center"
+  )
+    v-tab(
+      v-for="({ value, to, label }) in tabs"
+      :key="value"
+      v-bind="{ value, to }"
+    ) {{ label }}
+  router-view
 </template>
+
 <script setup>
 // imports
-import Container from '@/components/Layout/Container.vue'
 import { computed } from 'vue'
-import { RouterView, RouterLink } from 'vue-router'
+import { RouterView } from 'vue-router'
 import { useRoute } from 'vue-router'
+import Container from '@/components/Layout/Container.vue'
 
 // router
 const route = useRoute()
@@ -32,11 +26,13 @@ const route = useRoute()
 const tabs = computed(() => [
   {
     label: 'Login',
+    value: 'login',
     to: { name: 'login' },
     classes: route.name === 'login' ? 'is-active' : ''
   },
   {
     label: 'Register',
+    value: 'register',
     to: { name: 'register' },
     classes: route.name === 'register' ? 'is-active' : ''
   }
