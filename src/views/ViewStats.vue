87ae16bcf33fc554e066e91aee50b4a9cbe4802e
Mariusz <siewruk.mariusz92@gmail.com>
2023-02-10 14:43:32 +0100

    Refactored stats

--- src/views/ViewStats.vue
+++ src/views/ViewStats.vue
@@ -1,44 +1,22 @@
-<template>
-  <div class="stats">
-    <table class="table is-fullwidth">
-      <thead>
-        <tr>
-          <th>Stat</th>
-          <th>Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Number of Notes</td>
-          <td>{{ store.getNumberOfNotes }}</td>
-        </tr>
-        <tr>
-          <td>Number of character (all notes)</td>
-          <td>{{ store.getAllNotesCharacters }}</td>
-        </tr>
-      </tbody>
-    </table>
-    <input
-      v-model="comment"
-      type="text"
-      class="input"
-      placeholder="Add comment"
-      v-autofocus
-    />
-  </div>
+<template lang="pug">
+v-table
+  thead
+    tr
+      th Stat
+      th Value
+  tbody
+    tr
+      td Number of notes
+      td {{ store.getNumberOfNotes }}
+    tr
+      td Number of characters (all notes)
+      td {{ store.getAllNotesCharacters }}
 </template>
 
 <script setup>
-// imports
-import { ref } from 'vue'
-
 import { useNotesStore } from '@/stores/useNotesStore'
 import { vAutofocus } from '@/directives/vAutofocus'
 import { useWatchCharacters } from '@/use/useWatchCharacters'
-
-// comment
-const comment = ref('')
-useWatchCharacters(comment, 10)
 
 // store
 const store = useNotesStore()
