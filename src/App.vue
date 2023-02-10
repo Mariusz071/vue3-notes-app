a17b8f97e8c60fd30f5a8ea05e0112a30a7a7113
Mariusz <siewruk.mariusz92@gmail.com>
2023-02-10 11:21:50 +0100

    Refactored MainNav

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
-<template>
-  <MainNav />
-  <div class="container is-max-desktop px-2 py-4">
-    <Progressbar v-if="authStore.isLoading" />
-    <RouterView v-else />
-  </div>
+<template lang="pug">
+v-app
+  v-app-bar(
+    color="primary"
+    title="Notes App"
+  )
+    template(#prepend)
+      v-btn(
+        :icon="isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'"
+        @click="isOpen = !isOpen"
+      )
+  MainNav(v-model="isOpen")
+  v-main
+    v-card
+      Progressbar(v-if="authStore.isLoading")
+      RouterView(v-else)
 </template>
 
 <script setup>
 // imports
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import { RouterView } from 'vue-router'
 import MainNav from '@/components/Layout/MainNav.vue'
 import Progressbar from '@/components/Layout/Progressbar.vue'
@@ -17,10 +27,11 @@ import { useAuthStore } from '@/stores/useAuthStore'
 // store
 const authStore = useAuthStore()
 
+// nav
+const isOpen = ref('false')
+
 onMounted(() => {
   authStore.listenForAuthChanges()
 })
 </script>
-<style>
-@import 'bulma/css/bulma.min.css';
-</style>
+<style></style>
--- src/components/Layout/MainNav.vue
+++ src/components/Layout/MainNav.vue
@@ -1,116 +1,62 @@
-<template>
-  <nav
-    class="navbar is-success"
-    role="navigation"
-    aria-label="main navigation"
-  >
-    <div class="container is-max-desktop px-2">
-      <div class="navbar-brand ml-0">
-        <div class="navbar-item is-size-4 is-family-monospace px-0">
-          Noteballs
-        </div>
-
-        <a
-          v-if="storeAuth.user"
-          role="button"
-          ref="refNavbarBurger"
-          class="navbar-burger"
-          :class="{ 'is-active': isMobileNav }"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          @click.prevent="isMobileNav = !isMobileNav"
-        >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        class="navbar-menu"
-        ref="refMobileMenu"
-        :class="{ 'is-active': isMobileNav }"
-      >
-        <div
-          v-if="storeAuth.user"
-          class="navbar-start"
-        >
-          <button
-            :class="logoutBtnClasses"
-            @click="onLogout"
-          >
-            Log out {{ storeAuth.user.email }}
-          </button>
-        </div>
-        <div
-          v-if="storeAuth.user"
-          class="navbar-end"
-        >
-          <RouterLink
-            class="navbar-item"
-            active-class="is-active"
-            v-for="item in navItems"
-            :key="item.path"
-            :to="item.path"
-            @click="isMobileNav = false"
-          >
-            {{ item.label }}
-          </RouterLink>
-        </div>
-      </div>
-    </div>
-  </nav>
+<template lang="pug">
+v-navigation-drawer(
+  v-model="isOpen"
+  v-bind="{ location }"
+  app
+  dark
+)
+  v-list.pt-0
+    v-list-item(
+      v-for="item in navItems"
+      :key="item.path"
+      :active="item.isActive"
+      :to="item.path"
+      link
+    )
+      v-icon.mr-2 {{ item.icon }}
+      | {{ item.label }}
+    template(v-if="!!storeAuth.user")
+      v-divider
+      v-list-item(
+        @click="onLogout"
+      ) 
+        v-icon.mr-2 fa-solid fa-arrow-right-from-bracket
+        | Logout
+    
 </template>
 
 <script setup>
 // imports
 import { ref, computed } from 'vue'
-import { RouterLink } from 'vue-router'
-import { onClickOutside } from '@vueuse/core'
+import { useRoute } from 'vue-router'
+import { useDisplay } from 'vuetify'
 import { useAuthStore } from '@/stores/useAuthStore'
+
+// navigation
+const isOpen = ref(false)
+const route = useRoute()
+const { xs } = useDisplay()
+const location = computed(() => (xs.value ? 'bottom' : 'start'))
 
 // store
 const storeAuth = useAuthStore()
-const navItems = [
+const navItems = computed(() => [
   {
+    icon: 'far fa-note-sticky',
     label: 'Notes',
-    path: '/'
+    path: '/',
+    isActive: route.name === 'notes'
   },
   {
+    icon: 'fa-solid fa-chart-column',
     label: 'Stats',
-    path: '/stats'
+    path: '/stats',
+    isActive: route.name === 'stats'
   }
-]
-
-const isMobileNav = ref(false)
-const refMobileMenu = ref(null)
-const refNavbarBurger = ref(null)
-
-// handle click outside of menu
-onClickOutside(refMobileMenu, () => (isMobileNav.value = false), {
-  ignore: [refNavbarBurger]
-})
+])
 
 const onLogout = () => {
   storeAuth.onLogout()
   isMobileNav.value = false
 }
-
-// UI
-const logoutBtnClasses = computed(() => {
-  const baseClasses = ['button', 'is-small', 'is-info', 'mt-3', 'ml-3']
-  return useAuthStore.isLoading ? [...baseClasses, 'is-loading'] : baseClasses
-})
 </script>
-
-<style>
-@media (max-width: 1023px) {
-  .navbar-menu {
-    position: absolute;
-    left: 0;
-    width: 100%;
-  }
-}
-</style>
